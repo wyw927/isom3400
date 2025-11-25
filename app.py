efb172cebddf842b7e4e69166a57a0aa58b57eb3
wyw927 <112743929+wyw927@users.noreply.github.com>
2025-11-25 14:33:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+pip install matplotlib
+conda install matplotlib
 import matplotlib.pyplot as plt
 
 st.title("Data Analysis Dashboard")
